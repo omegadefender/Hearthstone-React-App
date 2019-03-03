ddc98afb2added9f17f1e54fa1ce040d3fb67f31
AirrQu0tes <hastings.james.m@gmail.com>
2019-03-04 00:27:08 +1100

    added routing, and expanded navbar menu

--- src/components/elements/Navbar.js
+++ src/components/elements/Navbar.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import '../../stylesheets/Navbar.css'
 
+import { Link } from "react-router-dom";
+
 class Navbar extends Component {
   render() {
     return (
       <nav>
         <ul>
-          <li>HOME</li>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>HS CARDS</li>
+          <li>ARENA TRACKER</li>
+          <li>STATS</li>
         </ul>
       </nav>
     );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import './stylesheets/index.css'
 import Homepage from './components/Pages/Homepage'
 
-ReactDOM.render(<Homepage />, document.getElementById('body'))
+ReactDOM.render((
+    <Router>
+        <switch>
+        <Route exact path="/" component={Homepage} />
+        </switch>
+    </Router>
+    ), document.getElementById('body')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
